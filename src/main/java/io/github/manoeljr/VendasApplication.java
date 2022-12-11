123a9e8a9810e6505d2ccfb656e14b4a83d4fa7e
manoeljr <manoelv8@gmail.com>
2022-12-10 23:16:41 -0300

    Criando as entities e repository

--- src/main/java/io/github/manoeljr/VendasApplication.java
+++ src/main/java/io/github/manoeljr/VendasApplication.java
@@ -1,12 +1,32 @@
 package io.github.manoeljr;
 
 
+import io.github.manoeljr.domain.entity.Cliente;
+import io.github.manoeljr.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired ClienteRepository clientes) {
+        return args -> {
+            clientes.salvar(new Cliente("Manoel"));
+            clientes.salvar(new Cliente("Outro cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
+
 }
